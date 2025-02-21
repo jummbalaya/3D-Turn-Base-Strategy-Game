649d9d20d9ea316dd3564db9999f97b5123ca6fb
jummbalaya <mite.shterjov@gmail.com>
2025-02-21 21:29:28 +0100

    expand the grid system

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,11 +9,19 @@ public class Unit : MonoBehaviour
 
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
     }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+
 
     private void Update()
     {
@@ -34,6 +42,15 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool("IsWalking", false);
         }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            // Unit changed Grid Position
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
     }
 
     public void Move(Vector3 targetPosition)
